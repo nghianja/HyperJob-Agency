d08980c7eb2df70a45947f9e7e781420f64d2bfa
James Ng <james.ng@projapps.com>
2020-09-26 23:52:24 +0800

    Complete Stage #1

--- HyperJob Agency/task/hyperjob/resume/models.py
+++ HyperJob Agency/task/hyperjob/resume/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Resume(models.Model):
+    description = models.CharField(max_length=1024)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
