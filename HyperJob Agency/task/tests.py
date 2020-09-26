6fa35a9b3ed63fb75051c2b6f567e87e26acd7d8
James Ng <james.ng@projapps.com>
2020-09-26 22:44:16 +0800

    Initial commit

---
+++ HyperJob Agency/task/tests.py
@@ -0,0 +1,22 @@
+from hstest.test_case import TestCase
+
+from base import HyperJobTest
+
+
+class HyperJobTestRunner(HyperJobTest):
+    def generate(self):
+        return [
+            # 1 task
+            TestCase(attach=self.check_server),
+            TestCase(attach=self.check_create_vacancies),
+            TestCase(attach=self.check_create_resumes),
+        ]
+
+    def check(self, reply, attach):
+        return attach()
+
+
+if __name__ == '__main__':
+    import os
+    os.environ['HYPERSKILL_TESTS_WITH_SQLITE3'] = '1'
+    HyperJobTestRunner('hyperjob.manage').run_tests()
